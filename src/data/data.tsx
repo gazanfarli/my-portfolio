03dce0e0ade8421c1f70d9bf1b534d191b34f5df
gazanfarli <qznfrli.2001@gmail.com>
2023-03-20 10:52:32 +0400

    adjusting portfolio section

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -13,17 +13,10 @@ import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import porfolioImage3 from '../images/portfolio/admin-dashboard.png';
+import porfolioImage2 from '../images/portfolio/headphones-ecommerce.png';
+import porfolioImage4 from '../images/portfolio/my-portfolio.png';
+import porfolioImage1 from '../images/portfolio/spacex-info.png';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -185,70 +178,28 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'SpaceX Info',
+    description: 'Discover Space-X\'s groundbreaking rocket technology.',
+    url: 'https://spacexinfo-az.netlify.app/',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Headphones E-commerce',
+    description: 'Experience immersive sound with our headphones',
+    url: 'https://headphones-ecommerce-rho.vercel.app/',
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'Admin Dashboard',
+    description: 'Streamline your operations with our dashboard',
+    url: 'https://admin-dashboard-brown-seven.vercel.app/',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    title: 'My Portfolio',
+    description: 'Showcasing my creative work and achievements',
+    url: 'https://gazanfarli.vercel.app/',
     image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage11,
   },
 ];
 
@@ -332,8 +283,8 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'gazanfarli@proton.me',
-      href: 'mailto:gazanfarli@proton.me',
+      text: 'qznfrli.2001@gmail.com',
+      href: 'mailto:qznfrli.2001@gmail.com',
     },
     {
       type: ContactType.Location,
