51db0d3efbb966475bddaf7e1a5b87bbd19a8a84
gazanfarli <qznfrli.2001@gmail.com>
2024-09-11 00:54:10 +0400

    fix lint issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,44 +1,44 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta
+          content="Welcome to the portfolio of Gazanfar Gazanfarli, a passionate Software Engineer from Baku, Azerbaijan. Specializing in React, Node.js, and mobile-first web applications. Explore my projects, skills, and professional journey."
           name="description"
-          content="Welcome to the portfolio of Gazanfar Gazanfarli, a passionate Software Engineer from Baku, Azerbaijan. Specializing in React, Node.js, and mobile-first web applications. Explore my projects, skills, and professional journey."
         />
         <meta
-          name="keywords"
           content="Gazanfar Gazanfarli, Software Engineer, Frontend Developer, React Developer, Web Developer, Azerbaijan, Portfolio, Mobile-first Web Applications, International Bank of Azerbaijan, Java Microservices, Backend-for-Frontend, Chakra UI, Tailwind CSS, React Native"
+          name="keywords"
         />
-        <meta name="author" content="Gazanfar Gazanfarli" />
+        <meta content="Gazanfar Gazanfarli" name="author" />
 
-        <meta property="og:title" content="Gazanfar Gazanfarli - Software Engineer | Portfolio Website" />
+        <meta content="Gazanfar Gazanfarli - Software Engineer | Portfolio Website" property="og:title" />
         <meta
-          property="og:description"
           content="Welcome to the portfolio of Gazanfar Gazanfarli, a passionate Software Engineer from Baku, Azerbaijan. Specializing in React, Node.js, and mobile-first web applications. Explore my projects, skills, and professional journey."
+          property="og:description"
         />
         <meta
+          content="https://drive.google.com/file/d/18XYRY68JZmiQoDS3TYje-CqWjJXLRP8i/view?usp=drive_link"
           property="og:image"
-          content="https://drive.google.com/file/d/18XYRY68JZmiQoDS3TYje-CqWjJXLRP8i/view?usp=drive_link"
         />
-        <meta property="og:url" content="https://gazanfarli.com" />
-        <meta property="og:type" content="website" />
+        <meta content="https://gazanfarli.com" property="og:url" />
+        <meta content="website" property="og:type" />
 
-        <meta name="twitter:card" content="Gazanfar Gazanfarli" />
-        <meta name="twitter:title" content="Gazanfar Gazanfarli - Software Engineer | Portfolio Website" />
+        <meta content="Gazanfar Gazanfarli" name="twitter:card" />
+        <meta content="Gazanfar Gazanfarli - Software Engineer | Portfolio Website" name="twitter:title" />
         <meta
-          name="twitter:description"
           content="Welcome to the portfolio of Gazanfar Gazanfarli, a passionate Software Engineer from Baku, Azerbaijan. Specializing in React, Node.js, and mobile-first web applications."
+          name="twitter:description"
         />
         <meta
-          name="twitter:image"
           content="https://drive.google.com/file/d/18XYRY68JZmiQoDS3TYje-CqWjJXLRP8i/view?usp=drive_link"
+          name="twitter:image"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link href="/favicon.ico" rel="icon" />
       </Head>
       <body>
         <Main />
